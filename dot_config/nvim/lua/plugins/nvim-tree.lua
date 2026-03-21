fe87812d910c58d3ee4b85f775483e5bcb8911da
m5lapp <64704718+m5lapp@users.noreply.github.com>
2026-03-21 19:16:45 +0000

    Configured nvim-tree to show hidden/.gitignored files.

--- dot_config/nvim/lua/plugins/nvim-tree.lua
+++ dot_config/nvim/lua/plugins/nvim-tree.lua
@@ -8,8 +8,13 @@ return {
   },
   config = function()
     require('nvim-tree').setup({
+      filters = {
+        -- `true` here means that the file types will be filtered out (hidden).
+        dotfiles = false,
+        git_ignored = false,
+      },
       view = {
-        width = 35,
+        width = 30,
       },
     })
     vim.keymap.set('n', '<C-n>', '<cmd>NvimTreeToggle<CR>', { desc = 'nvimtree toggle window' })
